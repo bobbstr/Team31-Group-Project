3fb84bb2534306d0c3a045f7f665c2ef03730d40
Samuel Devinett <230411191@aston.ac.uk>
2024-10-30 00:52:01 +0000

    Changed the sign up form to be more 'clean' and simple looking

--- signup.php
+++ signup.php
@@ -33,33 +33,36 @@ if (isset($_POST["submit"])) {
         <h1>Sugar Rush</h1>
     </header>
 
-    <section class="signup">
-        <h2>Sign up</h2>
     
-        <form action="/action_page.php">
-            <div>
-                <label for="FirstName">First Name</label>
-                <input type="text" id="FirstName" name="FirstName" placeholder="Name" required>
-            </div>
-            <div>
-                <label for="lname">Last Name</label>
-               <input type="text" id="lname" name="LastName" placeholder="Last Name" required>
-            </div>
-            <div>
-                <label for="email">Email</label>
-                <input type="text" id="email" name="Email" placeholder="Email" pattern=".+(\.ac\.uk|\.edu)" required>
-            </div>
-            <div>
-                <label for="Password">Password</label>
-                <input type="password" id="Password" name="Password" placeholder="Password" required>
-            </div>
-            
-            <div class="Sign up">
-                <input type="submit" value="Sign up">
-            </div>
+    <main class="content">
+        <section class="signup">
+            <h2>Sign up</h2>
+        
+            <form action="/action_page.php">
+                <div>
+                    <label for="FirstName">First Name</label>
+                    <input type="text" id="FirstName" name="FirstName" placeholder="Name" required>
+                </div>
+                <div>
+                    <label for="lname">Last Name</label>
+                <input type="text" id="lname" name="LastName" placeholder="Last Name" required>
+                </div>
+                <div>
+                    <label for="email">Email</label>
+                    <input type="text" id="email" name="Email" placeholder="Email" pattern=".+(\.ac\.uk|\.edu)" required>
+                </div>
+                <div>
+                    <label for="Password">Password</label>
+                    <input type="password" id="Password" name="Password" placeholder="Password" required>
+                </div>
+                
+                <div class="Sign up">
+                    <input type="submit" value="Sign up" id="regSubmit">
+                </div>
 
-            <p>Already have an account? <a href="login.html">Log in now</a></p>
-        </form>
-    </section>
+                <p id="loginLink">Already have an account? <a href="login.html">Log in now</a></p>
+            </form>
+        </section>
+    <main class="content">
 </body>
 </html>
