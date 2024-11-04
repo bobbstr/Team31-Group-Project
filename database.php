f9933c7308ff9a38ab21c8420edc12c66c8ce94f
Samuel <230411191@aston.ac.uk>
2024-11-04 15:10:53 +0000

    Changed database login details, so we can test run

--- database.php
+++ database.php
@@ -1,17 +1,16 @@
-    <?php
-        $server = "localhost";
-        $username = "user";
-        $password = "password";
-        $db_name = "SugarRush";
+<?php
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "sugarrush";
 
-        try{
-            $conn = mysqli_connect($server, $username, $password, $db_name);
-        }
-        catch(mysqli_sql_exception){
-            echo "could not connect!";
-        }
-        // Check database connection is working correctly.
-        if ($conn){
-            echo "You are connected!";
-        }
-    ?>
+    // Attempt to connect to the database
+    $conn = mysqli_connect($server, $username, $password, $db_name);
+
+    // Check database connection
+    if ($conn) {
+        echo "You are connected!";
+    } else {
+        echo "Could not connect: " . mysqli_connect_error();
+    }
+?>
