41c9b18dec04175030245b0efa92e7fa6c1c119f
Loveneet Singh <230125128@aston.ac.uk>
2025-03-04 23:35:05 +0000

    size of map

--- contactUs.php
+++ contactUs.php
@@ -141,7 +141,7 @@
 
   </form>
 
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2429.581261134505!2d-1.890822723951722!3d52.48671703869404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4870bc9ae4f2e4b3%3A0x9a670ba18e08a084!2sAston%20University!5e0!3m2!1sen!2suk!4v1741130935431!5m2!1sen!2suk" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2429.581261134505!2d-1.890822723951722!3d52.48671703869404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4870bc9ae4f2e4b3%3A0x9a670ba18e08a084!2sAston%20University!5e0!3m2!1sen!2suk!4v1741130935431!5m2!1sen!2suk" width="700" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
   <footer>
 
